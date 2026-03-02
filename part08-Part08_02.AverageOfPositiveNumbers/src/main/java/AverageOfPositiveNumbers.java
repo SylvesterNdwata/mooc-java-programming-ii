dc656b3d1b88038277cadad6d4773763f8a422a4
SylvesterNdwata <ndwatasylvester88@gmail.com>
2026-03-02 11:15:11 +0100

    Short recap

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,27 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int sum = 0;
+        while (true) {
+            String input = scanner.nextLine();
 
+            if (input.equals("0")) {
+                break;
+            }
+
+            int number = Integer.valueOf(input);
+            if (number > 0) {
+                sum += number;
+                count++;
+            }
+
+        }
+
+        if (sum == 0) {
+            System.out.println("Cannot calculate the average");
+        }
+
+        System.out.println(1.0 * sum / count);
     }
 }
--- part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,16 +5,56 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int first = 0;
+        int second = 0;
 
         while (true) {
             System.out.print("> ");
-
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
 
+            if (input.contains("add")) {
+                String[] parts = input.split(" ");
+                int amount = Integer.valueOf(parts[1]);
+
+                if (first + amount > 100) {
+                    first = 100;
+                } else if (amount > 0) {
+                    first += amount;
+                }
+            }
+            if (input.startsWith("move")) {
+                String[] parts = input.split(" ");
+                int amount = Integer.valueOf(parts[1]);
+                int toBeMoved = 0;
+                if (first <= amount) {
+                    amount = first;
+                }
+
+                if (amount + second > 100) {
+                    toBeMoved = 100 - second;
+                    second = 100;
+                    first -= toBeMoved;
+                } else {
+                    second += amount;
+                    first -= amount;
+                }
+            }
+
+            if (input.startsWith("remove")) {
+                String[] parts = input.split(" ");
+                int amount = Integer.valueOf(parts[1]);
+
+                if (amount > second) {
+                    second = 0;
+                } else if (second > 0) {
+                    second -= amount;
+                }
+            }
         }
     }
 
