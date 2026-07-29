852c57a40b1e834ea01a6ed7f0991b12049df4a6
SylvesterNdwata <ndwatasylvester88@gmail.com>
2026-07-29 12:53:22 +0200

    Class Diagrams

---
+++ part11-Part11_02.ABookAndAPlane/src/main/java/Plane.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ndwat
+ */
+public class Plane {
+    private String ID;
+    private String model;
+    private int yearOfIntroduction;
+
+    public Plane(String ID, String model, int yearOfIntroduction) {
+        this.ID = ID;
+        this.model = model;
+        this.yearOfIntroduction = yearOfIntroduction;
+    }
+}
---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ndwat
+ */
+public class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+    
+    public void play() {
+    }
+    
+    public void printName() {
+        System.out.println(this.name);
+    }
+}
