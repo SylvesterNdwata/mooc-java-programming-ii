70f95cd27ec4ca3c30e9414076befcf936a789bc
SylvesterNdwata <ndwatasylvester88@gmail.com>
2026-08-01 13:31:19 +0200

    Randomness

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,14 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        System.out.println("How many random numbers should be printed?");
+        int input = Integer.valueOf(scanner.nextLine());
+        
+        for (int i = 0; i < input; i++) {
+            System.out.println(random.nextInt(11));
+        }
     }
 
 }
