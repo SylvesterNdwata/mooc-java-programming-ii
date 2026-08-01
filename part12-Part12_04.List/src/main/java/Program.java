330c470526923fac65e21f5baf34fe0594d1eaac
SylvesterNdwata <ndwatasylvester88@gmail.com>
2026-08-01 12:10:38 +0200

    ArrayList and hash table

--- part12-Part12_04.List/src/main/java/Program.java
+++ part12-Part12_04.List/src/main/java/Program.java
@@ -7,6 +7,14 @@ public class Program {
         Scanner scan = new Scanner(System.in);
         // test your class here
 
+        List<String> myList = new List<>();
+        myList.add("hello");
+        myList.add("world");
+
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println(myList.value(i));
+        }
+
     }
 
 }
