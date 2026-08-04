5c7c25aac961996f0348fc55ab4aa358bb815ed6
SylvesterNdwata <ndwatasylvester88@gmail.com>
2026-08-04 13:01:32 +0200

    Event Handling

--- part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,40 @@
 package notifier;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class NotifierApplication {
+
+public class NotifierApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NotifierApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) {
+        
+        TextField textField = new TextField();
+        Button button = new Button("Update");
+        
+        Label label = new Label();
+        
+        button.setOnAction((event) -> {
+            label.setText(textField.getText());
+        });
+        
+        VBox componentGroup = new VBox();
+        componentGroup.getChildren().addAll(textField, button, label);
+        
+        Scene scene = new Scene(componentGroup);
+        
+        window.setScene(scene);
+        window.show();
     }
 
 }
--- part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,59 @@
 package textstatistics;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-public class TextStatisticsApplication {
+
+public class TextStatisticsApplication extends Application{
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) {
+        
+        BorderPane layout = new BorderPane();
+        
+        Label letters = new Label("Letters: ");
+        Label wordCount = new Label("Words: ");
+        Label longestWord = new Label("The longest word is: ");
+        TextArea area = new TextArea();
+        
+        HBox labels = new HBox();
+        labels.setSpacing(10);
+        labels.getChildren().add(letters);
+        labels.getChildren().add(wordCount);
+        labels.getChildren().add(longestWord);
+        
+        area.textProperty().addListener((text, oldValue, newValue) -> {
+            int characters = newValue.length();
+            String[] words = newValue.split(" ");
+            String longest = Arrays.stream(words)
+                    .max(Comparator.comparing(String::length))
+                    .get();
+            
+            letters.setText("Letters: " + characters);
+            wordCount.setText("Words: " + words.length);
+            longestWord.setText("The longest word is: " + longest);
+        });
+        
+        layout.setBottom(labels);
+        
+        layout.setCenter(area);
+        
+        Scene view = new Scene(layout);
+        
+        window.setScene(view);
+        window.show();
     }
 
 }
