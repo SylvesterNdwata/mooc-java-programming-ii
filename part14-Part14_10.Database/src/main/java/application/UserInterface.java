fd38092eed96a1b822a3c5e09d4d9a13077a5c5d
SylvesterNdwata <ndwatasylvester88@gmail.com>
2026-08-16 10:07:20 +0200

    Database

--- part14-Part14_10.Database/src/main/java/application/UserInterface.java
+++ part14-Part14_10.Database/src/main/java/application/UserInterface.java
@@ -30,6 +30,37 @@ public class UserInterface {
             }
 
             // implement the functionality here
+            switch (command) {
+                case "1":
+                    System.out.println("Listing the database contents");
+                    for (Todo todo : this.database.list()) {
+                        System.out.println(todo);
+                    }
+                    break;
+
+                case "2":
+                    System.out.println("Adding a new todo");
+                    System.out.println("Enter name ");
+                    String name = scanner.nextLine();
+                    System.out.println("Enter description ");
+                    String description = scanner.nextLine();
+                    int id = this.database.list().size();
+                    Todo toAdd = new Todo(id, name, description, false);
+                    this.database.add(toAdd);
+                    break;
+
+                case "3":
+                    System.out.println("Which todo should be marked as done (give the id)?");
+                    int idToMark = Integer.valueOf(scanner.nextLine());
+                    this.database.markAsDone(idToMark);
+                    break;
+
+                case "4":
+                    System.out.println("Which todo should be removed (give the id)?");
+                    int idToDelete = Integer.valueOf(scanner.nextLine());
+                    this.database.remove(idToDelete);
+                    break;
+            }
         }
 
         System.out.println("Thank you!");
